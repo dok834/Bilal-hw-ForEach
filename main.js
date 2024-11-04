266382e05c1e016a622d6a69658cbeb685b6b2be
dok834 <evgenez43@gmail.com>
2024-11-04 17:55:23 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//     //Д\з ForEch
+//     1)
+// const dom = ['яблоко', 'банан', 'апельсин']
+// dom.forEach((i)=>{
+//     console.log(i)
+// })
+
+//2)
+// const dom = [1,2,3]
+// dom.forEach((i)=>{
+//     console.log(i*2)
+// })
+
+//3)
+// const dom = [5,10,15]
+// let dom1 = 0
+// dom.forEach(function(dom){
+//     dom1+=dom
+// })
+// console.log(dom1)
+
+//4)
+// const dom = ['привет', 'мир']
+// dom.forEach(function(word){
+//     console.log(word.toUpperCase())
+// })
+
+//5)
+// const dom = [2, 5, 8, 1, 10,6,11,15]
+// let dom1 = 0
+// dom.forEach(function(dom){
+//     for(i = 0;i <= dom;i++){
+//         if(i>5){
+//             console.log(dom)
+//         }
+//     }
+// })
